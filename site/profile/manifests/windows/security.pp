c0405d2055f8d367b7106729c25fd003da24816b
kkarthee <k.karthee@gmail.com>
2017-12-29 11:39:35 +0800

    .

--- site/profile/manifests/windows/security.pp
+++ site/profile/manifests/windows/security.pp
@@ -7,13 +7,13 @@ class profile::windows::security (
   registry::value { 'IE Enhanced security user':
     key   => "${key_loc}\\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}",
     value =>  'IsInstalled',
-    data  => '0',
+    data  => '1',
     type  => dword,
 }
 registry::value { 'IE Enhanced security Admin':
     key   => "${key_loc}\\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}",
     value => 'IsInstalled',
     type  =>  dword,
-    data  =>  '0',
+    data  =>  '1',
 }
 }

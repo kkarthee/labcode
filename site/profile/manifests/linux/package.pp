190d197f0ea23d1392a64271591557e35f6f567d
kkarthee <k.karthee@gmail.com>
2017-12-29 16:09:50 +0800

    package &ntp

---
+++ site/profile/manifests/linux/package.pp
@@ -0,0 +1,7 @@
+class profile::linux::package (
+  pacakge_install = lookup ( 'package', {merge =>  unique})
+)
+{
+package { $pacakge_install:
+  ensure =>  installed,
+}
--- site/role/manifests/linux.pp
+++ site/role/manifests/linux.pp
@@ -1,2 +1,4 @@
 class role::linux {
+  include profile::linux::package
+  include profile::linux::ntpcon
 }

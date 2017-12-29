e5d62006e9fc7a6330f016daf670e8ad4b51b779
kkarthee <k.karthee@gmail.com>
2017-12-29 10:53:52 +0800

    seciitty

--- site/profile/manifests/windows/security.pp
+++ site/profile/manifests/windows/security.pp
@@ -1,16 +1,17 @@
 class profile::windows::security (
-  $adminkey = 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
-  $userkey = 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}'
+  # $adminkey = 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}',
+  #$userkey = 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}'
+  $key_loc = 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components'
 )
 {
   registry::value { 'IE Enhanced security user':
-    key   => $userkey,
+    key   => "${key_loc}\\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}",
     value =>  'IsInstalled',
     data  => '0',
     type  => dword,
 }
 registry::value { 'IE Enhanced security Admin':
-    key   => $adminkey,
+    key   => "${adminkey}\\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}",
     value => 'IsInstalled',
     type  =>  dword,
     data  =>  '0',

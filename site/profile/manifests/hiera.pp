5991f09ea569c044b6f1a878bf4c1845338e403c
kkarthee <k.karthee@gmail.com>
2017-12-29 13:30:12 +0800

    .

--- site/profile/manifests/hiera.pp
+++ site/profile/manifests/hiera.pp
@@ -1,15 +1,16 @@
 class  profile::hiera (
 )
 {
-class { 'hiera': 
+class { 'hiera':
     hiera_version   =>   '5',
     hiera5_defaults => {'datadir' => '/etc/puppetlabs/code/environments/%{::environment}/data', 'data_hash' => 'yaml_data'},
     hierarchy       =>   [
     {'name' => 'Virtual yaml','path'  =>  'virtual/%{::virtual}.yaml'},
     {'name' => 'Nodes yaml', 'path' =>  'nodes/%{::trusted.certname}.yaml'},
     {'name' => 'OS yaml', 'path' =>  'os/%{::osfamily}.yaml'},
+    {'name' => 'kernel yaml', 'path' =>  'os/%{::kernel}.yaml'},
     {'name' => 'Default yaml file', 'path' =>  'global.yaml'},
     ],
-    eyaml  => true,
+    eyaml           => true,
 }
 }

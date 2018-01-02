5d081167de0c9a123d34988afdf38974d0c41ece
kkarthee <k.karthee@gmail.com>
2018-01-02 11:20:21 +0800

    message

--- site/profile/manifests/linux/ntpcon.pp
+++ site/profile/manifests/linux/ntpcon.pp
@@ -1,13 +1,5 @@
 class profile::linux::ntpcon (
-  $ntp_server = lookup ('ntp_server_list', { merge => unique})
 )
 {
-class  { 'ntp':
-    servers    => $ntp_server,
-    autoupdate => false,
-    enable     =>  true,
-  }
-  include ntp::install
-  include ntp::service
+  include ntp
 }
-

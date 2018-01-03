f4fc14cb7cc224b50271a7250202aa4f457a7b42
kkarthee <k.karthee@gmail.com>
2018-01-03 16:30:42 +0800

    sudo

---
+++ site/profile/manifests/linux/user.pp
@@ -0,0 +1,14 @@
+class profile::linux::user (
+  $myuserkey = lookup ( 'profile::linux::user::myuser' )
+)
+{
+  user { 'myuser':
+    ensure         => present,
+    purge_ssh_keys => true,
+    managehome     => true,
+    key            => $myserkey,
+  }
+  include sudo
+  include sudo::configs
+}
+

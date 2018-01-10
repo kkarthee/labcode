63200048f54d6532abee823886266a4ca7d8aabc
kkarthee <k.karthee@gmail.com>
2018-01-10 17:37:05 +0800

    updated puppetfile and removed modules

--- site/aws/examples/basic-cloudformation-example/cloudformation-01-create.pp
+++
@@ -1,6 +0,0 @@
-
-cloudformation_stack { 's3-bucket-test':
-  ensure        => updated,
-  region        => 'us-west-2',
-  template_url  => 'https://s3-us-west-2.amazonaws.com/cloudformation-templates-us-west-2/S3_Website_Bucket_With_Retain_On_Delete.template',
-}
--- site/aws/examples/basic-cloudformation-example/cloudformation-03-destroy.pp
+++
@@ -1,6 +0,0 @@
-
-cloudformation_stack { 's3-bucket-test':
-  ensure       => absent,
-  region       => 'us-west-2',
-}
-

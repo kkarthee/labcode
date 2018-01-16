93bb50881af2a937a60bee2a981326d36357a5b6
kkarthee <k.karthee@gmail.com>
2018-01-16 17:34:40 +0800

    .

--- site/profile/manifests/windows/file.pp
+++ site/profile/manifests/windows/file.pp
@@ -3,13 +3,13 @@ class profile::windows::file (
 {
   file { 'C:\\temp\\usertest':
     ensure =>  directory,
-    owner  =>  'usertest',
+    owner  =>  'testuser',
     group  =>  'testgroup',
     mode   =>   '0755',
 }
   file {'C:\\temp\\usertest\\test.txt':
     ensure  =>  'file',
-    owner   =>  'usertest',
+    owner   =>  'testuser',
     group   =>  'testgroup',
     mode    =>  '0740',
     #source  =>  'C:\\temp\\test',

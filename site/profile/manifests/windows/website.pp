ca547c849909f3978d507122fa251ed5bdbf8ecf
kkarthee <k.karthee@gmail.com>
2018-01-25 17:17:51 +0800

    .

--- site/profile/manifests/windows/website.pp
+++ site/profile/manifests/windows/website.pp
@@ -1,47 +1,20 @@
 class profile::windows::website {
-# dsc_windowsfeature {'IIS':
-
-# dsc_ensure =>  'present',
-
-# dsc_name   =>  'Web-Server',
-
-# }
-
-# dsc_windowsfeature{'aspnet45':
-
-# dsc_ensure =>  'Present',
-
-# dsc_name =>  'Web-Asp-Net45',
-
-# }
-
-# iis_site { 'Test Web Site':
-
-# ensure   =>  'started',
-
-# app_pool =>  'DefaultAppPool',
-
-# ip       =>  '*',
-
-# path     =>  'C:\temp\wwroot',
-
-# port     =>  '80',
-
-# protocol =>  'http',
-
-# ssl      =>  false,
-
-# }
 $iis_features = ['Web-WebServer','Web-Scripting-Tools']
 
 iis_feature { $iis_features:
     ensure =>  'present',
 }
     -> iis_site { 'Test Webserver':
-    ensure          =>  'started',
-    physicalpath    =>  'c:\\temp\\wwwroot',
-    applicationpool =>  'DefaultAppPool',
-    require         =>  File['webdir'],
+    ensure          => 'started',
+    physicalpath    => 'c:\\temp\\wwwroot',
+    applicationpool => 'DefaultAppPool',
+    require         => File['webdir'],
+    bindings        =>  [
+          {
+                  'bindinginformation' =>  '*:8080:',
+                        'protocol'     =>  'http',
+          },
+    ],
 }
 
 file { 'webdir':

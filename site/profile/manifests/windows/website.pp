af5c8df1e7f0e3b74c4ada78005e87cac33a2808
kkarthee <k.karthee@gmail.com>
2018-01-25 16:46:08 +0800

    .

--- site/profile/manifests/windows/website.pp
+++ site/profile/manifests/windows/website.pp
@@ -1,19 +1,51 @@
 class profile::windows::website {
-dsc_windowsfeature {'IIS':
-    dsc_ensure =>  'present',
-    dsc_name   =>  'Web-Server',
+# dsc_windowsfeature {'IIS':
+
+# dsc_ensure =>  'present',
+
+# dsc_name   =>  'Web-Server',
+
+# }
+
+# dsc_windowsfeature{'aspnet45':
+
+# dsc_ensure =>  'Present',
+
+# dsc_name =>  'Web-Asp-Net45',
+
+# }
+
+# iis_site { 'Test Web Site':
+
+# ensure   =>  'started',
+
+# app_pool =>  'DefaultAppPool',
+
+# ip       =>  '*',
+
+# path     =>  'C:\temp\wwroot',
+
+# port     =>  '80',
+
+# protocol =>  'http',
+
+# ssl      =>  false,
+
+# }
+$iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+iis_feature { $iis_features:
+    ensure =>  'present',
 }
-dsc_windowsfeature{'aspnet45':
-    dsc_ensure =>  'Present',
-      dsc_name =>  'Web-Asp-Net45',
+    -> iis_site { 'Test Webserver':
+    ensure          =>  'started',
+    physicalpath    =>  'c:\\inetpub\\minimal',
+    applicationpool =>  'DefaultAppPool',
+    require         =>  File['webdir'],
 }
-iis_site { 'Test Web Site':
-    ensure   =>  'started',
-    app_pool =>  'DefaultAppPool',
-    ip       =>  '*',
-    path     =>  'C:\temp\wwroot',
-    port     =>  '80',
-    protocol =>  'http',
-    ssl      =>  false,
+
+file { 'webdir':
+    ensure =>  'directory',
+      path =>  'c:\\temp\\wwwroot',
 }
 }

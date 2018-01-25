9d9d29335a1d1c228c161a828c2e974eea80e59d
kkarthee <k.karthee@gmail.com>
2018-01-25 15:04:15 +0800

    .

--- site/profile/manifests/windows/website.pp
+++ site/profile/manifests/windows/website.pp
@@ -3,6 +3,10 @@ dsc_windowsfeature {'IIS':
     dsc_ensure =>  'present',
     dsc_name   =>  'Web-Server',
 }
+dsc_windowsfeature{'aspnet45':
+    dsc_ensure =>  'Present',
+      dsc_name =>  'Web-Asp-Net45',
+}
 iis_site { 'Test Web Site':
     ensure   =>  'started',
     app_pool =>  'DefaultAppPool',

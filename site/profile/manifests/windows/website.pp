1c764c18e74cbd4269d7288db775d753797422c2
kkarthee <k.karthee@gmail.com>
2018-01-28 18:08:55 +0800

    .

--- site/profile/manifests/windows/website.pp
+++ site/profile/manifests/windows/website.pp
@@ -2,25 +2,20 @@ class profile::windows::website (
 $iis_features = ['Web-WebServer','Web-Scripting-Tools']
 )
 {
-  file { 'webdir':
-          ensure =>   'directory',
-          path   =>   'c:\\temp\\wwwroot',
-                    }
-iis_feature { $iis_features:
-    ensure =>  'present',
-}
-    -> iis_site { 'Test Webserver':
-    ensure          => 'started',
-    physicalpath    => 'c:\\temp\\wwwroot',
-    applicationpool => 'DefaultAppPool',
-    require         => File['webdir'],
-    #provider        => 'powershell',
-    bindings        => [
-          {
-                  'bindinginformation' =>  '*:8080:',
-                        'protocol'     =>  'http',
-          },
-    ],
-}
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
 
+  iis_feature { $iis_features:
+      ensure =>  'present',
+  }
+  -> iis_site { 'minimal':
+      ensure          =>  'started',
+      physicalpath    =>  'c:\\inetpub\\minimal',
+      applicationpool =>  'DefaultAppPool',
+      require         =>  File['minimal'],
+  }
+
+  file { 'minimal':
+      ensure =>  'directory',
+        path =>  'c:\\inetpub\\minimal',
+  }
 }

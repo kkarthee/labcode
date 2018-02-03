a3df54ff36453173d864d401c5636b2de274d8ce
kkarthee <k.karthee@gmail.com>
2018-02-03 23:39:31 +0800

    .

--- site/role/manifests/windows.pp
+++ site/role/manifests/windows.pp
@@ -4,6 +4,6 @@ class role::windows {
     include profile::windows::user
     include profile::windows::file
     #include profile::windows::acl
-    include profile::windows::website
+    #    include profile::windows::website
     include profile::windows::archive
 }

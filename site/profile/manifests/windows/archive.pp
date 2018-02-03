9a5edcf1a7c9793be7bfe85776f43ab934a038c8
kkarthee <k.karthee@gmail.com>
2018-02-04 00:02:51 +0800

    .

--- site/profile/manifests/windows/archive.pp
+++ site/profile/manifests/windows/archive.pp
@@ -1,8 +1,13 @@
 class profile::windows::archive
 {
+  file { '7z1801-x64.msi':
+    path   => 'c:\\Temp\\7z1801-x64.msi',
+    source =>  'puppet:///modules/custom/7z1801-x64.msi',
+  }
   class { 'archive':
     seven_zip_name     =>  '7-Zip 9.20 (x64 edition)',
-      seven_zip_source =>  'C:/Windows/Temp/7z920-x64.msi',
+      seven_zip_source =>  'C:/Windows/Temp/7z1801-x64.msi',
     seven_zip_provider =>  'windows',
+    require           => File[ '7z1801-x64.msi'],
   }
 }

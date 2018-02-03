772a7d4663838eb3a1d53adf048a50f5f5019a19
kkarthee <k.karthee@gmail.com>
2018-02-03 23:37:24 +0800

    .

---
+++ site/profile/manifests/windows/archive.pp
@@ -0,0 +1,8 @@
+class profile::windows::archive
+{
+  class { 'archive':
+    seven_zip_name     =>  '7-Zip 9.20 (x64 edition)',
+      seven_zip_source =>  'C:/Windows/Temp/7z920-x64.msi',
+    seven_zip_provider =>  'windows',
+  }
+}
--- site/role/manifests/windows.pp
+++ site/role/manifests/windows.pp
@@ -5,4 +5,5 @@ class role::windows {
     include profile::windows::file
     #include profile::windows::acl
     include profile::windows::website
+    include profile::windows::archive
 }

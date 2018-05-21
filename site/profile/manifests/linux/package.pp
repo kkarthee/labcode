ca19c2c737e9f0a2f1dfc5e30af13494d7b6d9b5
Karthikeyan <k.karthee@gmail.com>
2018-05-21 11:23:06 +0800

    Update package.pp

--- site/profile/manifests/linux/package.pp
+++ site/profile/manifests/linux/package.pp
@@ -1,11 +1,12 @@
 class profile::linux::package (
-  $pacakge_install = lookup ( 'package', {merge =>  unique})
+# $pacakge_install = lookup ( 'package', {merge =>  unique})
 )
 {
   package { 'yum':
     ensure =>  latest,
   }
-package { $pacakge_install:
+#package { $pacakge_install:
+package { vim:
   ensure  => latest,
   require =>  Package['yum'],
 }

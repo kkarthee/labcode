f43bc4fa38d194b2d71930773b85e9746d1ba8dd
Karthikeyan <k.karthee@gmail.com>
2018-10-29 15:57:34 +0800

    Update windows.pp

--- site/role/manifests/windows.pp
+++ site/role/manifests/windows.pp
@@ -1,9 +1,9 @@
 class role::windows {
     include profile::windows::param
-    include profile::windows::base
-    include profile::windows::user
-    include profile::windows::file
+    #include profile::windows::base
+    #include profile::windows::user
+   # include profile::windows::file
     #include profile::windows::acl
     #    include profile::windows::website
-    include profile::windows::archive
+  #  include profile::windows::archive
 }
